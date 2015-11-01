c4fcc92dde882698583e23e0653ea03f3e9811b0
undefinederror <nowtypethis@gmail.com>
2015-11-01 20:02:55 +0100

    some polishing

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 ﻿var _ = require('lodash');
 
-function cdn() {
+function cdn(conf) {
     var cdn = {
         DEFAULTS: JSON.parse(JSON.stringify(require('./lib/const.js').DEFAULTS))
     };
     _.extend(cdn, { utils: require('./lib/utils.js') }, require('./lib/methods.js'));
+    if (conf) {cdn.setValues(conf)}
     return cdn;
 }
-
 
 module.exports = cdn;
